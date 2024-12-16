8ac43f3e14eeb194808b7f2e9b157e4ffb174c3f
archie <57256763+Bl4omArchie@users.noreply.github.com>
2024-12-16 01:43:50 +0100

    Update install_snort.sh

--- install_snort.sh
+++ install_snort.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-#####################################################################
-#                   ~ Snort installation script ~                   #
-#                                                                   #
-# Author : archie                                                   #
-# Description : install snort3 for linux using apt package manager  #
-#####################################################################
+
+###################################################################################################
+#                               ~ Snort installation script ~
+# Author : archie
+# Description : install snort3, with gperftools and hyperscan,  for linux using apt package manager 
+###################################################################################################
 
 
-#depedencies
-required_packages=('cmake' 'libdaq-dev' 'libdnet-dev' 'flex' 'g++' 'hwloc' 'libluajit-5.1-dev' 'libssl-dev' 'libpcap-dev' 'libpcre3-dev' 'pkg-config' 'zlib1g-dev')
-
-#default path for snort3 repository and libdaq library
-snort3_repository_path="/home/cryptx/snort3"
-libdaq_repository_path="/home/cryptx/libdaq"
-libdaq_lib_path="/usr/local/lib/daq_s3"
 
 #colors
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0m"
 
+#depedencies
+snort_required_packages=('cmake' 'libdaq-dev' 'libdnet-dev' 'flex' 'g++' 'hwloc' 'libluajit-5.1-dev' 'libssl-dev' 'libpcap-dev' 'libpcre3-dev' 'pkg-config' 'zlib1g-dev')
+hypperscan_required_packages=('cmake' 'ragel' 'libboost-all-dev' 'build-essential')
 
 
 install_depedencies() {
-    for i in "${required_packages[@]}"
+    for i in "$1"
     do
         if ! dpkg -s "${i}" >/dev/null 2>&1; then
             sudo apt install "${i}" -y -qq
@@ -34,95 +30,73 @@ install_depedencies() {
     return 1
 }
 
-install_libdaq() {
-    cd /home/cryptx
-    if ! git clone -q https://github.com/snort3/libdaq.git; then 
-        echo -e "${RED}[✘] Error : couldn't get libdaq${NC}"
+install_snort() {
+    install_depedencies $snort_required_packages
+    if ! git clone -q https://github.com/snort3/snort3.git; then 
+        echo -e "${RED}[✘] Error : couldn't get snort3 repository${NC}"
         return 0
     fi
+    cd snort3
+    ./configure_cmake.sh --prefix=/usr/local --enable-tcmalloc
+    cd build
+    make -j 8 -s
+    sudo make install -s
+    sudo ldconfig
+    echo -e "${GREEN}[✔] Snort3 installed !${NC}" 
+}
 
+install_hyperscan() {
+    install_depedencies $hypperscan_required_packages
+    if  ! git clone -q https://github.com/intel/hyperscan.git; then 
+        echo -e "${RED}[✘] Error : couldn't get hyperscan repository${NC}"
+        return 0
+    fi
+    cd hyperscan
+    mkdir build && cd build
+    cmake ..
+    make -j $(nprocs) -s
+    sudo make install -s
+    echo -e "${GREEN}[✔] Hyperscan installed !${NC}" 
+}
+
+install_gperftools() {
+    wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.9.1/gperftools-2.9.1.tar.gz
+    tar xzf gperftools-2.9.1.tar.gz
+    cd gperftools-2.9.1/
+    ./configure
+    make -j $(nprocs) -s
+    sudo make install -s
+    echo -e "${GREEN}[✔] Gperftools installed !${NC}" 
+}
+
+install_libdaq() {
+    if ! git clone -q https://github.com/snort3/libdaq.git; then 
+        echo -e "${RED}[✘] Error : couldn't get libdaq repository${NC}"
+        return 0
+    fi
     cd libdaq
     ./bootstrap
-    ./configure --prefix=/usr/local/lib/daq_s3
-    make install 
-    sudo ldconfig
-
+    ./configure
+    make -j $(nprocs) -s
+    sudo make install -s
     echo -e "${GREEN}[✔] Libdaq installed !${NC}" 
-    return 1
 }
 
-# args :
-# $1 (optional) = path_install_snort
-# If you don't indicates manually the path, the default one will be used.
-install_snort3() {
-    cd /home/cryptx
-    if ! git clone -q https://github.com/snort3/snort3.git; then
-        echo -e "${RED}[✘] Error : couldn't get snort3${NC}"
-        exit 1
-    fi
+start_snort_installation() {
+    #mkdir ~/snort_src && cd ~/snort_src
+    #install_libdaq
 
-    mkdir -p "${snort3_repository_path}"
-    cd snort3
-    ./configure_cmake.sh --prefix="${snort3_repository_path}"
+    #cd ~/snort_src
+    #install_gperftools
 
-    cd build
-    make -j $(nproc)
-    make install
+    cd ~/snort_src
+    install_hyperscan
 
-    "${snort3_repository_path}"/bin/snort -V
-    "${snort3_repository_path}"/bin/snort --daq-list
+    cd ~/snort_src
+    install_snort
 
-    echo -e "${GREEN}[✔] Snort3 installed !${NC}" 
-    return 1
+    snort -V
 }
 
 
-verify_paths() {
-    res=0
-    if [ ! -d "$snort3_path" ]; then
-        echo -e "${RED}[✘] Snort3 is missing. Update path or use install_snort3${NC}"
-        res+=1
-    fi
-
-    if [ ! -d "$libdaq_path" ]; then
-        echo -e "${RED}[✘] Libdaq is missing. Update path or use install_libdaq${NC}"
-        res+=1
-    fi
-    return $res
-}
-
-install() {
-    if install_depedencies == 0; then
-        return 0
-    elif install_libdaq == 0; then
-        return 0
-    elif install_snort3 == 0; then
-        return 0
-    fi
-    return 1
-}
-
-remove() {
-    if [ -d "${snort3_repository_path}" ]; then
-        sudo rm -rf "${snort3_repository_path}"
-        echo -e "${GREEN}[✔] Snort3 repository correcly removed !${NC}"
-    fi
-
-    if [ -d "${libdaq_repository_path}" ]; then
-        sudo rm -rf "${libdaq_repository_path}"
-        echo -e "${GREEN}[✔] Libdaq repository correcly removed !${NC}"
-    fi
-
-    if [ -d "${libdaq_lib_path}" ]; then
-        sudo rm -rf "${libdaq_lib_path}"
-        echo -e "${GREEN}[✔] Libdaq library correcly removed !${NC}"
-    fi
-}
-
-main() {
-    remove
-    install
-}
-
-
-main
+start_snort_installation
