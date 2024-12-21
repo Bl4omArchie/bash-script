0bb142d1c84bf2c1bba3435c2586952ba5fabdca
Bl4omArchie <grlch.alexandre@proton.me>
2024-12-21 17:42:33 +0100

    install_snort works with hyperscan

--- install_snort.sh
+++ install_snort.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
-#####################################################################
-#                   ~ Snort installation script ~                   #
-#                                                                   #
-# Author : archie                                                   #
-# Description : install snort3 for linux using apt package manager  #
-#####################################################################
+
+
+# ---
+# Snort3 installer using apt package manager
+# 
+# Manage : installation, update and removal
+# Add-on : hyperscan for fast pattern matching, more to come
+#
+# Author : archie - 2024
+# ---
 
 
 
-required_packages=('cmake' 'libdaq-dev' 'libdnet-dev' 'flex' 'g++' 'hwloc' 'libluajit-5.1-dev' 'libssl-dev' 'libpcap-dev' 'libpcre3-dev' 'pkg-config' 'zlib1g-dev')
-
-#default path for snort3 repository and libdaq library
-snort3_repository_path="~/"
-libdaq_repository_path="~/"
-libdaq_lib_path="/usr/local/lib/daq_s3"
-
+#colors
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0m"
 
+#depedencies
+snort_required_packages=('cmake'            #snort requirements
+                        'libdaq-dev' 
+                        'libdnet-dev' 
+                        'flex' 
+                        'g++' 
+                        'hwloc' 
+                        'libluajit-5.1-dev' 
+                        'libssl-dev' 
+                        'libpcap-dev' 
+                        'libpcre3-dev' 
+                        'pkg-config'
+                        'zlib1g-dev'
+                        'ragel'            #hyperscan requirements
+                        'build-essential'
+                        'libboost-all-dev'
+                        'libhyperscan-dev')
 
+
+set_variables() {
+    snort_default_path="/usr/local/snort"
+    daq_default_path="/usr/local/lib/daq_s3"
+}
 
 install_depedencies() {
-    for i in "${required_packages[@]}"
+    for i in "$1"
     do
         if ! dpkg -s "${i}" >/dev/null 2>&1; then
             sudo apt install "${i}" -y -qq
@@ -33,85 +53,60 @@ install_depedencies() {
     return 1
 }
 
-install_libdaq() {
-    cd "${libdaq_repository_path}"
-    if ! git clone -q https://github.com/snort3/libdaq.git; then 
-        echo -e "${RED}[✘] Error : couldn't get libdaq${NC}"
+install_snort() {
+    install_depedencies $snort_required_packages
+    if ! git clone -q https://github.com/snort3/snort3.git; then 
+        echo -e "${RED}[✘] Error : couldn't get snort3 repository${NC}"
         return 0
     fi
+    cd snort3
+    ./configure_cmake.sh --prefix=${snort_default_path} --with-daq-includes=${daq_default_path}/include/ --with-daq-libraries=${daq_default_path}/lib/
+    cd build
+    make -j $(( $(nproc) / 2 )) -s
+    sudo make install -s
+    sudo ldconfig
+    echo -e "${GREEN}[✔] Snort3 installed !${NC}" 
+}
 
+install_libdaq() {
+    if ! git clone -q https://github.com/snort3/libdaq.git; then 
+        echo -e "${RED}[✘] Error : couldn't get libdaq repository${NC}"
+        return 0
+    fi
     cd libdaq
     ./bootstrap
-    echo "${libdaq_lib_path}"
-    ./configure --prefix="${libdaq_lib_path}"
-    make install
-    sudo ldconfig
-
+    ./configure
+    make -j $(( $(nproc) / 2 )) -s
+    sudo make install -s
     echo -e "${GREEN}[✔] Libdaq installed !${NC}" 
-    return 1
 }
 
-#args :
-# $1 (optional) = path_install_snort
-# If you don't indicates manually the path, the default one will be used. Which is ~/
-install_snort3() {
-    cd "${snort3_repository_path}"
-    if ! git clone -q https://github.com/snort3/snort3.git; then
-        echo -e "${RED}[✘] Error : couldn't get snort3${NC}"
-        exit 1
-    fi
-
-    export snort3_installation="${snort3_repository_path}"
-    mkdir -p $snort3_installation
-    cd snort3
-    ./configure_cmake.sh --prefix="${snort3_installation}"
-
-    cd build
-    make -j $(nproc)
-    make install
-
-    $"{snort3_installation}"bin/snort -V
-
-    echo -e "${GREEN}[✔] Snort3 installed !${NC}" 
-    return 1
+remove_snort_installation() {
+    sudo rm -rf ${snort_default_path}
 }
 
 
-verify_paths() {
-    res=0
-    if [ ! -d "$snort3_path" ]; then
-        echo -e "${RED}[✘] Snort3 is missing. Update path or use install_snort3${NC}"
-        res+=1
-    fi
-
-    if [ ! -d "$libdaq_path" ]; then
-        echo -e "${RED}[✘] Libdaq is missing. Update path or use install_libdaq${NC}"
-        res+=1
-    fi
-    return $res
+remove_libdaq_installation() {
+    sudo rm -rf ${daq_default_path}
 }
 
-install() {
-    if install_depedencies == 0; then
-        return 0
-    elif install_libdaq == 0; then
-        return 0
-    elif install_snort3 == 0; then
-        return 0
-    fi
-    return 1
+remove_snort() {
+    remove_snort_installation
+    remove_libdaq_installation
+    sudo rm -rf ~/snort_src
 }
 
-remove() {
-    sudo rm -rf "${snort3_repository_path}"
-    sudo rm -rf "${libdaq_lib_path}"
-    echo -e "${GREEN}[✔] Snort3 correcly removed !${NC}" 
-}
+install_snort() {
+    set_variables
 
-main() {
-    remove
-    install
+    mkdir ~/snort_src && cd ~/snort_src
+    install_libdaq
+
+    cd ~/snort_src
+    install_snort
+    snort -V
 }
 
 
-main
+remove_snort
+install_snort
