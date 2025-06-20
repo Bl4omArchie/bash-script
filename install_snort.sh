37fd06d61e370b18ef1e9983fb4a99f4e30339e8
archie <57256763+Bl4omArchie@users.noreply.github.com>
2025-06-20 14:46:54 +0200

    major fixes

--- install_snort.sh
+++ install_snort.sh
@@ -6,7 +6,7 @@
 # Snort3 installer using apt package manager
 # 
 # Manage : installation, update and removal
-# Add-on : hyperscan for fast pattern matching, more to come
+# Add-on : hyperscan for fast pattern matching
 #
 # Author : archie - 2024
 # ---
@@ -20,11 +20,10 @@ NC="\033[0m"
 
 #depedencies
 snort_required_packages=('cmake'            #snort requirements
-                        'libdaq-dev' 
-                        'libdnet-dev' 
+                        'libdaq-dev'
                         'flex' 
                         'g++' 
-                        'libhwloc-dev' 
+                        'hwloc' 
                         'libluajit-5.1-dev' 
                         'libssl-dev' 
                         'libpcap-dev' 
@@ -38,12 +37,12 @@ snort_required_packages=('cmake'            #snort requirements
 
 
 set_variables() {
+    snort_install="${HOME}/snort_repo"
     snort_default_path="/usr/local/snort"
     daq_default_path="/usr/local/lib/daq_s3"
 }
 
 install_depedencies() {
-    for i in "$1"
     for i in "$1"
     do
         if ! dpkg -s "${i}" >/dev/null 2>&1; then
@@ -54,24 +53,8 @@ install_depedencies() {
     return 1
 }
 
-install_snort() {
-    install_depedencies $snort_required_packages
-    if ! git clone -q https://github.com/snort3/snort3.git; then 
-        echo -e "${RED}[✘] Error : couldn't get snort3 repository${NC}"
-        return 0
-    fi
-    cd snort3
-    ./configure_cmake.sh --prefix=${snort_default_path} --with-daq-includes=${daq_default_path}/include/ --with-daq-libraries=${daq_default_path}/lib/
-    cd build
-    make -j $(( $(nproc) / 2 )) -s
-    sudo make install -s
-    sudo ldconfig
-    echo -e "${GREEN}[✔] Snort3 installed !${NC}" 
-}
-
 install_libdaq() {
     if ! git clone -q https://github.com/snort3/libdaq.git; then 
-        echo -e "${RED}[✘] Error : couldn't get libdaq repository${NC}"
         echo -e "${RED}[✘] Error : couldn't get libdaq repository${NC}"
         return 0
     fi
@@ -83,35 +66,46 @@ install_libdaq() {
     ./configure
     make -j $(( $(nproc) / 2 )) -s
     sudo make install -s
+    sudo ldconfig
     echo -e "${GREEN}[✔] Libdaq installed !${NC}" 
 }
 
-remove_snort_installation() {
-    sudo rm -rf ${snort_default_path}
-}
-
-
-remove_libdaq_installation() {
-    sudo rm -rf ${daq_default_path}
+install_snort() {
+    if ! git clone -q https://github.com/snort3/snort3.git; then 
+        echo -e "${RED}[✘] Error : couldn't get snort3 repository${NC}"
+        return 0
+    fi
+    cd snort3
+    ./configure_cmake.sh --prefix=${snort_default_path} --with-daq-includes=${daq_default_path}/include/ --with-daq-libraries=${daq_default_path}/lib/
+    cd build
+    make -j $(( $(nproc) / 2 )) -s
+    sudo make install -s
+    
+    echo 'export PATH=$PATH:/usr/local/snort/bin' >> ${HOME}/.bashrc
+    source ${HOME}/.bashrc
+    echo -e "${GREEN}[✔] Snort3 installed !${NC}" 
 }
 
 remove_snort() {
-    remove_snort_installation
-    remove_libdaq_installation
-    sudo rm -rf ~/snort_src
+    sudo rm -rf ${snort_default_path}
+    sudo rm -rf ${daq_default_path}
+    sudo rm -rf ${snort_install}
+    echo -e "Snort has been removed"
 }
 
-install_snort() {
+start_install() {
     set_variables
 
-    mkdir ~/snort_src && cd ~/snort_src
+    install_depedencies $snort_required_packages
+
+    mkdir ${snort_install} && cd ${snort_install}
     install_libdaq
 
-    cd ~/snort_src
+    cd ${snort_install}
     install_snort
     snort -V
 }
 
 
 remove_snort
-install_snort
+start_install
